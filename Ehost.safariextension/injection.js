8ed971151e751fd2d67810a0345ff46294a05de3
Sijmen Mulder <sjmulder@gmail.com>
2011-09-05 20:19:01 +0200

    Added page-wide context menu support to supported sites

--- Ehost.safariextension/injection.js
+++ Ehost.safariextension/injection.js
@@ -2,13 +2,35 @@
 // Published under the BSD licence.
 
 (function() {
-	var handleContextMenu = function(event) {
-		var userInfo = {};
-		userInfo.isUploadableImage = event.target.nodeName === "IMG";
-		if (userInfo.isUploadableImage) {
-			userInfo.imageUrl = event.target.src;
+	// <link rel="image_src" href="…">
+	// used by imgur, TwitPic, PhotoBucket, Flickr, Twitgoo
+	var getImageSrcLinkImage = function() {
+		var imageSrcLink = document.querySelector('link[rel="image_src"]');
+		return imageSrcLink ? imageSrcLink.href : null;
+	};
+
+	// <meta property="og:image" value="…">
+	// used by yFrog
+	var getOpenGraphImage = function() {
+		var metaTags = document.querySelectorAll('meta[property]');
+		for (var i = 0; i < metaTags.length; i++) {
+			var metaTag = metaTags[i];
+			if (metaTag.getAttribute('property') === 'og:image') {
+				return metaTag.getAttribute('content');
+			}
 		}
-		safari.self.tab.setContextMenuEventUserInfo(event, userInfo);
+
+		return null;
+	};
+
+	var findImageUrl = function() {
+		return getImageSrcLinkImage() || getOpenGraphImage();
+	};
+	
+	var handleContextMenu = function(event) {
+		var imgNodeSrc = event.target.nodeName == "IMG" ? event.target.src : null;
+		var imageUrl = imgNodeSrc || findImageUrl();
+		safari.self.tab.setContextMenuEventUserInfo(event, { imageUrl: imageUrl });
 	};
 
 	document.addEventListener("contextmenu", handleContextMenu, false);
